18b0d1820e264d86d63fe75ebf9b61a2d9c5ab18
Tolipov <ziyovuddin.developer@gmail.com>
2024-04-28 03:22:00 +0500

    user done ✅

--- src/components/UserVote.tsx
+++ src/components/UserVote.tsx
@@ -2,7 +2,7 @@
 const Results = () => {
     return (
         <div className="text-primary-200 results">
-            <h2 className="text-3xl font-semibold text-primary-200 text-center">Lavozimlarga tayinlov.</h2>
+            <h2 className="text-3xl font-semibold text-primary-200 text-center">Dekan lavozimiga tayinlov.</h2>
             <ul className="results_list">
                 <li data-aos="flip-down" >Muhammadali Aliksey </li>
                 <li data-aos="flip-down" >Aliksey Aliksey 2</li>
